923aac1d8cabb388366a4e728033dd76680fc7d4
satoshi.yamamoto <standard.software.net@gmail.com>
2022-04-20 20:00:16 +0900

    update view

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
 
-  const prefArray = ['北海道', '青森', '岩手'];
+  const [prefArray, setPrefArray] = useState([]);
 
   fetch(
     `https://opendata.resas-portal.go.jp/api/v1/prefectures`,
     {
       headers: {"x-api-key": "-"}
     }).then(res => {
-    console.log({res});
+    // console.log({res});
     return res.json();
   }).then(json => {
-    console.log({json});
+    // console.log({json});
+    const result = [];
+    for (const {prefCode, prefName} of json.result) {
+      result.push(prefName)
+    }
+    setPrefArray(result);
   });
 
   return (
@@ -21,11 +27,13 @@ function App() {
         <div>
           都道府県
         </div>
+        <div style={{    overflowY: 'scroll', height: '200px'}}>
         {prefArray.map(pref => {
           return (<>
             {pref}<br />
           </>)
         })}
+        </div>
       </header>
     </div>
   );
