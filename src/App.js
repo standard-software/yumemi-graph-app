dc32c5d831335708e7dc4efd0bf9b7d495021eeb
satoshi.yamamoto <standard.software.net@gmail.com>
2022-04-20 21:58:44 +0900

    update graph

--- src/App.js
+++ src/App.js
@@ -17,49 +17,93 @@ const optionsHighcharts = {
   ]
 }
 
+const API_KEY = "-"
+
 function App() {
 
-  const [refreshData, setRefreshData] = useState(false);
+  const [refreshPref, setRefreshPref] = useState(false);
+  const [refreshGraph, setRefreshGraph] = useState(false);
 
   const [prefArray, setPrefArray] = useState([
-    {prefName: '北海道', checked: false},
-    {prefName: '青森', checked: true},
-    {prefName: '岩手', checked: false},
+    {prefCode: '1', prefName: '北海道', checked: false},
+    {prefCode: '2', prefName: '青森', checked: true},
+    {prefCode: '3', prefName: '岩手', checked: false},
   ]);
 
   useEffect(() => {
-    console.log('useEffect');
-    if (refreshData === false) { return; }
+    console.log('useEffect 1');
+    if (refreshPref === false) { return; }
     fetch(
       `https://opendata.resas-portal.go.jp/api/v1/prefectures`,
       {
-        headers: {"x-api-key": "-"}
+        headers: {"x-api-key": API_KEY}
       }).then(res => {
       // console.log({res});
       return res.json();
     }).then(json => {
       console.log({json});
       const result = [];
-      for (const {prefName} of json.result) {
-        result.push({prefName, checked: false});
+      for (const {prefCode, prefName} of json.result) {
+        result.push({prefCode, prefName, checked: false});
       }
       setPrefArray(result);
     });
-    setRefreshData(false);
-  }, [refreshData])
+    setRefreshPref(false);
+  }, [refreshPref])
+
+  useEffect(() => {
+    console.log('useEffect 2');
+    if (refreshGraph === false) { return; }
+
+    const f = async () => {
+
+      const results = [];
+
+      for (const {prefName, prefCode, checked} of prefArray) {
+        if (!checked) { continue; }
+
+        console.log({prefName, prefCode, checked})
+
+        const parameter = `prefCode=${prefCode}`;
+
+        results.push(
+          fetch(
+            `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-1&${parameter}`,
+            {
+              headers: {"x-api-key": API_KEY}
+            }).then(res => {
+            // console.log({res});
+            return res.json();
+          }).then(json => {
+            console.log({json});
+            // const result = [];
+            // for (const {prefName} of json.result) {
+            //   result.push({prefName, checked: false});
+            // }
+            // setPrefArray(result);
+          })
+        );
+      }
+      await Promise.all(results)
+      console.log(`after Promise.all`);
+
+      setRefreshGraph(false);
+    };
+    f();
+
+  }, [prefArray, refreshGraph])
 
   return (
     <div className="App">
       <header className="App-header">
         <div onClick={() => {
-          console.log('都道府県');
-          setRefreshData(true);
+          setRefreshPref(true);
         }}>
           都道府県
         </div>
         <div style={{    overflowY: 'scroll', height: '200px'}}>
         {prefArray.map(({prefName, checked}) => {
-          console.log({prefName, checked})
+          // console.log({prefName, checked})
           return (<>
             <label>
               <input
@@ -78,6 +122,11 @@ function App() {
           </label>
           </>)
         })}
+        </div>
+        <div onClick={() => {
+          setRefreshGraph(true);
+        }}>
+          グラフ
         </div>
         <HighchartsReact
           highcharts={Highcharts}
